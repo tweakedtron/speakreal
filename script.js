07f9a23b9d5fdc106e239fe6c4a5d9a37be8ef41
tweakedtron <tweakedtron@gmail.com>
2026-04-03 23:33:58 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const apiKey = "AIzaSyDO6ZVI4nHZBTZRUoAp0U6JDlVpnEFsqSg"; // 👈 thay bằng key của m
+const apiKey = AIzaSyDO6ZVI4nHZBTZRUoAp0U6JDlVpnEFsqSg; // 👈 thay bằng key của m
 
 const btnSubmit = document.getElementById("btn-submit");
 const inputText = document.getElementById("input-text");
